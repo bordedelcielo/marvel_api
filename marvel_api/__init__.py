16202c6e802a650f472c25272e236253547659e0
bordedelcielo <christopher.thrutchley@gmail.com>
2021-11-12 13:06:43 -0600

    fixes to __init__.py for flask db init keyerror

--- marvel_api/__init__.py
+++ marvel_api/__init__.py
@@ -4,6 +4,11 @@ from .site.routes import site
 from .authentication.routes import auth
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from .models import db as root_db
+
 app = Flask(__name__)
 
 app.register_blueprint(api)
@@ -11,3 +16,10 @@ app.register_blueprint(site)
 app.register_blueprint(auth)
 
 app.config.from_object(Config)
+
+root_db.init_app(app)
+
+migrate = Migrate(app, root_db)
+
+#import models
+# If the above import models statement exists in the file, flask db init will fail.
