03bd54614935b691a39facb5e2d62aa42a034e0b
bordedelcielo <christopher.thrutchley@gmail.com>
2021-11-12 21:32:09 -0600

    reviewed

--- marvel_api/api/routes.py
+++ marvel_api/api/routes.py
@@ -8,3 +8,76 @@ api = Blueprint('api', __name__, url_prefix = '/api')
 @token_required
 def getdata(current_user_token):
     return { 'some': 'value'}
+
+# CRUD operations
+
+# Create
+@api.route('/heroes', methods = ['POST'])
+@token_required
+def create_hero(current_user_token):
+    name = request.json['name']
+    description = request.json['description']
+    comics_appeared_in = request.json['comics_appeared_in']
+    super_power = request.json['super_power']
+    date_created = request.json['date_created']
+    owner = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    hero = Hero(name,description,comics_appeared_in,super_power,date_created, owner = owner)
+
+    db.session.add(hero)
+    db.session.commit()
+
+    response = hero_schema.dump(hero)
+    return jsonify(response)
+
+# Retrive all
+@api.route('/heroes', methods = ['GET'])
+@token_required
+def get_heroes(current_user_token):
+    owner_hash = current_user_token.token # maybe I should choose a different name than "owner".
+    heroes = Hero.query.filter_by(owner = owner_hash)
+    response = heroes_schema.dump(heroes)
+    return jsonify(response)
+
+# Retrive one hero endpoint
+@api.route('/heroes/<id>', methods = ['GET'])
+@token_required
+def get_drone(current_user_token, id):
+    owner_hash = current_user_token.token
+    if owner_hash == current_user_token.token:
+        hero = Hero.query.get(id)
+        response = hero_schema.dump(hero)
+        return jsonify(response)
+    else:
+        return jsonify({'message': 'Valid token required'}), 401
+
+# UPDATE
+@api.route('/heroes/<id>', methods = ['POST', 'PUT'])
+@token_required
+def update_hero(current_user_token,id):
+    hero = Hero.query.get(id)
+
+    # hold alt to click at beginning of each line and add hero.
+
+    hero.name = request.json['name'] 
+    hero.description = request.json['description']
+    hero.comics_appeared_in = request.json['comics_appeared_in']
+    hero.super_power = request.json['super_power']
+    hero.date_created = request.json['date_created']
+    hero.owner = current_user_token.token
+
+    db.session.commit()
+    response = hero_schema.dump(hero)
+    return jsonify(response)
+
+# DELETE
+@api.route('/heroes/<id>', methods = ['DELETE'])
+@token_required
+def delete_hero(current_user_token, id):
+    hero = Hero.query.get(id)
+    db.session.delete(hero)
+    db.session.commit()
+    response = hero_schema.dump(hero)
+    return jsonify(response)
