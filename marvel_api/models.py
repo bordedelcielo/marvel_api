94b01156cb88aeca3176ac94f85f1199eb8c63c4
bordedelcielo <christopher.thrutchley@gmail.com>
2021-11-12 21:19:36 -0600

    reviewed

--- marvel_api/models.py
+++ marvel_api/models.py
@@ -2,18 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import uuid
 from datetime import datetime
-
 from werkzeug.security import generate_password_hash, check_password_hash
-
 import secrets
-
 from flask_login import UserMixin, LoginManager #originally two lines of from flask_login imports.
-
 from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-ma = Marshmallow
+ma = Marshmallow()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -25,18 +21,20 @@ class User(db.Model, UserMixin): # added UserMixin for day_3 branch
     first_name = db.Column(db.String(150), nullable = True, default='')
     last_name = db.Column(db.String(150), nullable = True, default ='')
     email = db.Column(db.String(150), nullable = False)
+    user_name = db.Column(db.String, nullable = True)
     password = db.Column(db.String, nullable = True, default = '')
     g_auth_verify = db.Column(db.Boolean, default = False)
     token = db.Column(db.String, default = '', unique = True)
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
     hero = db.relationship('Hero', backref = 'owner', lazy = True)
 
-    def __init__(self,email,first_name='',last_name='',id='',password='',token='',g_auth_verify=False):
+    def __init__(self,email,user_name,first_name='',last_name='',id='',password='',token='',g_auth_verify=False):
         self.id = self.set_id()
         self.first_name = first_name
         self.last_name = last_name
-        self.password = self.set_password(password)
         self.email = email
+        self.user_name = user_name
+        self.password = self.set_password(password)
         self.token = self.set_token(24)
         self.g_auth_verify = g_auth_verify
 
@@ -51,7 +49,7 @@ class User(db.Model, UserMixin): # added UserMixin for day_3 branch
         return self.pw_hash
 
     def __repr__(self):
-        return f'User {self.email} has been added to the database!'
+        return f'User {self.user_name} has been added to the database!'
 
 class Hero(db.Model):
     id = db.Column(db.Integer, primary_key = True) # change Integer to String if you run into problems.
