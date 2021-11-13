f0c0ba3ee249d9b357415b9b0fd2cf8cfd5936c0
bordedelcielo <christopher.thrutchley@gmail.com>
2021-11-12 21:14:37 -0600

    reviewed

--- marvel_api/forms.py
+++ marvel_api/forms.py
@@ -6,8 +6,8 @@ class UserSignupForm(FlaskForm):
     # email, password, submit_button
     first_name = StringField('First Name', validators = [DataRequired()])
     last_name = StringField('Last Name', validators = [DataRequired()])
-    user_name = StringField('Username', validators = [DataRequired()])
     email = StringField('Email', validators = [DataRequired(), Email()])
+    user_name = StringField('Username', validators = [DataRequired()])
     password = PasswordField('Password', validators = [DataRequired()])
     submit_button = SubmitField()
 
